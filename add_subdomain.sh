a806b1cd289fe8fcebc7a70171d1d2414eb1f9b4
Michiel@unhosted <michiel@unhosted.org>
2013-06-11 12:44:55 +0200

    as per irc chat

--- add_subdomain.sh
+++ add_subdomain.sh
@@ -2,10 +2,10 @@
 
 SUBDOMAIN=$1
 NS="heahdk.net."
-KEYFILE="Kadmin.un.ht.+157+53670.private""
+KEYFILE="Kadmin.un.ht.+157+53670.private"
 
 nsupdate -k $KEYFILE -v <<EOF 
-server un.ht
+server localhos
 zone un.ht
 update add $SUBDOMAIN.un.ht. 3600 NS $NS
 show
