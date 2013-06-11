b7bda3d75fc3a5171882b2f7a8f4c9949f3a0551
Michiel@unhosted <michiel@unhosted.org>
2013-06-11 13:54:53 +0200

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,4 @@
 ssh bind_admin@heahdk.net "cd dns-scripts; ./create_zone.sh $1" | head -1
 ./add_subdomain.sh $1 > /dev/null
+#ssh root@heahdk.net "useradd $2"
+#ssh web_admin@heahdk.net "./create_site.sh $1 $2"
