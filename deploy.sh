b756e104c00d307cff42e1270e2e0ad3f92302f8
bind_admin <bind_admin@heahdk.net>
2013-06-12 10:59:35 +0000

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 ssh bind_admin@heahdk.net "cd dns-scripts; ./create_zone.sh $1" | head -1
-./add_subdomain.sh $1 > /dev/null
-#ssh root@heahdk.net "useradd $2"
+/home/customer-backend/add_subdomain.sh $1 > /dev/null
+#sudo /usr/sbin/adduser --disabled-password --disabled-login --gecos '' --uid $2 rs$2
 #ssh web_admin@heahdk.net "./create_site.sh $1 $2"
