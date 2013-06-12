59f3782d39fd4ad8cad959f0182b113cdfd9d27e
Michiel@unhosted <michiel@unhosted.org>
2013-06-12 14:36:16 +0300

    Update websetup.sh

--- websetup.sh
+++ websetup.sh
@@ -1,2 +1,3 @@
 sudo mkdir -p /home/rs$2/storage/www/$1.un.ht/
 echo "echo \"<html><h1>Welcome to $1.un.ht</h1></html>\" > /home/rs$2/storage/www/$1.un.ht/index.html" | sudo su
+ln -s /home/rs$2/storage/www/$1.un.ht /home/michiel/www/$1.un.ht
